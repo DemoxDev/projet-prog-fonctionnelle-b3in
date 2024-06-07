ca1e6e95ded2f296480181d31468768b4dbbe5f3
DemoxDev <chrisdoesroblox@gmail.com>
2024-06-07 18:17:04 +0200

    Refactor professorModel pour inclure courseSessions

--- src/models/professorModel.tsx
+++ src/models/professorModel.tsx
@@ -1,6 +1,9 @@
+import CourseSessionModel from './courseSessionModel';
+
 export interface ProfessorModel {
     id: number;
     firstName: string;
     lastName: string;
-  }
+    courseSessions: CourseSessionModel[];
+}
   
